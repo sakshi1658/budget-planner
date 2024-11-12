c283925423ceaceef6c3c9ccbe34a42dccee6db7
sakshi1658 <sharmasakshi1658@gmail.com>
2024-11-12 11:18:04 +0530

    login component generate and route add for login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'budget-planner',
+    loadChildren: () =>
+      import('./budget-planner/budget-planner-routing.module').then(
+        (m) => m.BudgetPlannerRoutingModule
+      ),
+  },
+];
