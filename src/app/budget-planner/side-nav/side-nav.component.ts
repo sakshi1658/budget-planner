09b125d1a2a5a9b5b0fc1f6ad2a5a2fbca63621c
sakshi1658 <sharmasakshi1658@gmail.com>
2024-11-13 10:09:36 +0530

    mat-icon import and toggle-button add

--- src/app/budget-planner/side-nav/side-nav.component.ts
+++ src/app/budget-planner/side-nav/side-nav.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
+  isSlideOut = true;
 
+  toggleSlideOut(): void {
+    this.isSlideOut = !this.isSlideOut;
+  }
 }
