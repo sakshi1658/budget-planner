9b7cab0725dd727d74e099573db15b5288647b80
sakshi1658 <sharmasakshi1658@gmail.com>
2024-11-13 09:55:16 +0530

    sidebar componnet generate and routed

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent }];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'side-nav', component: SideNavComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
