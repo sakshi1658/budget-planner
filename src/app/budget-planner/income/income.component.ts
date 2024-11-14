9cfb198cda7f41ef26e1d1a87d21730f3e353fd2
sakshi1658 <sharmasakshi1658@gmail.com>
2024-11-14 16:53:23 +0530

    added income form and onsubmit functio

--- src/app/budget-planner/income/income.component.ts
+++ src/app/budget-planner/income/income.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-income',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './income.component.html',
   styleUrl: './income.component.scss'
 })
 export class IncomeComponent {
+  incomeForm:any;
+  constructor(public fb:FormBuilder){}
+  ngOnInit(): void {
+    this.incomeForm = this.fb.group({
+      month: ['', Validators.required],
+      source: ['', Validators.required],
+      amount: ['', Validators.required],
+      investments: ['', Validators.required],
+    });
+  }
+
+  onSubmit(){
+    
+  }
 
 }
